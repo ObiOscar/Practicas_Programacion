510e43dc39c6eb6ed44edf807920e43bd3fa09d5
ObiOscar <oscar.fer.rodz@gmail.com>
2016-10-07 17:03:08 +0200

    Modificación de programa para que salga edad

---
+++ practica2_0607.java
@@ -0,0 +1,9 @@
+public class practica2_0607 {
+	
+	
+	public static void main(String args[]){
+		System.out.println("Nombre alumno: Oscar Fernandez Rodriguez");
+		System.out.println("Edad alumno: 24 años");
+		
+	}
+}
